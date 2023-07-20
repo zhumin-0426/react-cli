b89c1877c21a228279a7e3ea161b6f7d1ba45860
Darian <971367636@qq.com>
2023-07-20 16:45:38 +0800

    feat(package): 添加postcss&legacy插件

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { visualizer } from "rollup-plugin-visualizer";
+import legacy from '@vitejs/plugin-legacy'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    legacy({
+      targets: ['defaults', 'not IE 11'],
+    }),
     visualizer()
   ],
 })
