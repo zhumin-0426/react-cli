d34533c2139399ab58345cab4e57381540944f93
Darian <971367636@qq.com>
2023-07-21 23:05:20 +0800

    refactor:(vite): 添加相关配置

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,24 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { visualizer } from "rollup-plugin-visualizer";
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    visualizer()
-  ],
+export default defineConfig(({ command, mode }) => {
+  return {
+    resolve:{
+      alias:{
+        '@':'/src'
+      }
+    },
+    server:{
+      proxy:{
+        //https://github.com/http-party/node-http-proxy#options
+        '/apiDevlopment':{},
+        '/apiTest':{},
+        '/apiBuild':{}
+      }
+    },
+    plugins: [
+      react(),
+      visualizer()
+    ],
+  }
 })
