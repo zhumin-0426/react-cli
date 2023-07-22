c11fb4748f10dff393dc6767c45f154b45be09aa
Darian <971367636@qq.com>
2023-07-22 16:41:05 +0800

    feat(css): 添加postcss-nesting

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,9 @@
 // const postcssPresetEnv = require("postcss-preset-env");
 import postcssPresetEnv from 'postcss-preset-env'
-
+import postcssNested from 'postcss-nested'
 export default {
     plugins:[
+        postcssNested(),
         postcssPresetEnv()
     ]
 }
