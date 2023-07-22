d569206de10b96ccb54f0e93b3370fd80acc66d5
Darian <971367636@qq.com>
2023-07-22 16:07:23 +0800

    feat(css): 添加postcss支持

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+// const postcssPresetEnv = require("postcss-preset-env");
+import postcssPresetEnv from 'postcss-preset-env'
+
+export default {
+    plugins:[
+        postcssPresetEnv()
+    ]
+}
