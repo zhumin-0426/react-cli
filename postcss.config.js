f785ddf83c2d947cc1bf5d60c67f664eb8407a8e
Darian <971367636@qq.com>
2023-07-23 19:49:23 +0800

    fix(postcss): 引入出错

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 // const postcssPresetEnv = require("postcss-preset-env");
 import postcssPresetEnv from 'postcss-preset-env'
-import postcssNested from 'postcss-nested'
+import postcssNested from 'postcss-nesting'
 export default {
     plugins:[
         postcssNested(),
